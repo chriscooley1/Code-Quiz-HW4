774a6e87c62a19d0f491751caee0a973a8073a63
Chris Cooley <chriscooley1984@yahoo.com>
2020-11-09 11:01:08 -0700

    added variables and questions

---
+++ script.js
@@ -0,0 +1,15 @@
+var startButton = document.getElementById ("start")
+var questionsArray = [
+    {
+        title: "What's 9 + 10",
+        possibleAnswers: ["19", "21", "7", "34"],
+        answer: "19"
+    },
+    {
+        title: "What's 2 + 4",
+        possibleAnswers: ["6", "21", "7", "34"],
+        answer: "6"
+    },
+]
+
+// click button starts timer, also unhides first question

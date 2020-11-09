3ac4a9fbc42555597e1f383e775bedfd326b1435
Chris Cooley <chriscooley1984@yahoo.com>
2020-11-09 16:20:14 -0700

    added answers to questions in index.js

--- script.js
+++ script.js
@@ -5,27 +5,27 @@ var questionsArray = [
     {
         title: "Commonly used data types DO Not include:",
         possibleAnswers: ["1. strings", " 2. booleans", " 3. alerts", " 4. numbers"],
-        answer: "6"
+        answer: "3. alerts"
     },
     {
         title: "The condition in an if/else statement is enclosed within ____.",
         possibleAnswers: ["1. quotes", " 2. curly brackets", " 3. parenthesis", " 4. square brackets"],
-        answer: "6"
+        answer: "3. parenthesis"
     },
     {
         title: "Arrays in Javascript can be used to store ____.",
         possibleAnswers: ["1. numbers and strings", " 2. other arrays", " 3. booleans", " 4. all of the above"],
-        answer: "6"
+        answer: "4. all of the above"
     },
     {
         title: "String values must be enclosed within ____ when being assigned to variables.",
         possibleAnswers: ["1. commas", " 2. curly brackets", " 3. quotes", " 4. parenthesis"],
-        answer: "6"
+        answer: "3. quotes"
     },
     {
         title: "A very useful tool used during development and debugging for printing content to the debugger is:",
         possibleAnswers: ["1. Javascript", " 2. terminal/bash", " 3. for loops", " 4. console.log"],
-        answer: "6"
+        answer: "4. console.log"
     },
 ]
 

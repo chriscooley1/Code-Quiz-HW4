a8888aa57b199cc4d63a56de8224f0dc3a84cd95
Chris Cooley <chriscooley1984@yahoo.com>
2020-11-10 21:36:33 -0700

    added answer1-4 variables and separated each answer

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
 var startButton = document.getElementById ("start");
 var questionsEl = document.getElementById ("question");
+var answer1 = document.getElementById("answer1");
+var answer2 = document.getElementById("answer2");
+var answer3 = document.getElementById("answer3");
+var answer4 = document.getElementById("answer4");
+var nextButton = document.getElementById("next-button");
 var questionIndex = 0
 var questionsArray = [
     {
         question: "Commonly used data types DO Not include:",
-        possibleAnswers: ["strings", " booleans", " alerts", " numbers"],
-        correctAnswer: "alerts"
+        answer1: "strings",
+        answer2: "booleans",
+        answer3: "alerts",
+        answer4: "numbers",
+        correctAnswer: "alerts",
     },
     {
         question: "The condition in an if/else statement is enclosed within ____.",
-        possibleAnswers: ["quotes", " curly brackets", " parenthesis", " square brackets"],
-        answer: "parenthesis"
+        answer1: "quotes",
+        answer2: "curly brackets",
+        answer3: "parenthesis",
+        answer4: "square brackets",
+        correctAnswer: "parenthesis",
     },
     {
         question: "Arrays in Javascript can be used to store ____.",
-        possibleAnswers: ["numbers and strings", " other arrays", " booleans", " all of the above"],
-        answer: "all of the above"
+        answer1: "numbers and strings",
+        answer2: "other arrays",
+        answer3: "booleans",
+        answer4: "all of the above",
+        correctAnswer: "all of the above",
     },
     {
         question: "String values must be enclosed within ____ when being assigned to variables.",
-        possibleAnswers: ["commas", " curly brackets", " quotes", " parenthesis"],
-        answer: "quotes"
+        answer1: "commas",
+        answer2: "curly brackets",
+        answer3: "quotes",
+        answer4: "parenthesis",
+        correctAnswer: "quotes",
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        possibleAnswers: ["Javascript", " terminal/bash", " for loops", " console.log"],
-        answer: "console.log"
+        answer1: "Javascript",
+        answer2: "terminal/bash",
+        answer3: "for loops",
+        answer4: "console.log",
+        correctAnswer: "console.log",
     },
 ]
 
@@ -47,7 +67,7 @@ function getQuestion () {
     var questionChoice = document.getElementById ("choices")
     var answer = document.getElementById ("answer")
     questionTitle.textContent = currentQuestion.question
-    questionChoice.textContent = currentQuestion.possibleAnswers
+    questionChoice.textContent = currentQuestion.answer1
     answer.textContent = currentQuestion.correctAnswer
 }
 // click button starts timer, also unhides first question
